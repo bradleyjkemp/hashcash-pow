7f3cf3bbaa6758b0cfbce459be7ad1b867542006
bradleyjkemp <scytheon3@gmail.com>
2017-03-12 16:00:05 +0000

    Implement token generation

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
 "use strict";
 let Hashcash = function() {};
+let sha = require('sha.js');
 
-if (typeof(window) === "undefined") {
-  let crypto = require('crypto');
-  Hashcash.prototype.getHash = function (string, callback) {
-    callback(crypto.createHash("sha1").update(string).digest());
-  };
-} else {
-  Hashcash.prototype.getHash = function (string, callback) {
-    window.crypto.subtle.digest('SHA-1', new window.TextEncoder('utf-8').encode(string))
-    .then(function (hash) {
-      callback(hash);
-    });
-  };
-}
-
-Hashcash.prototype.checkToken = function (token, callback) {
+Hashcash.prototype.checkToken = function (token) {
   let tokenArray;
   let tokenString;
   if (typeof(token) == "string") {
@@ -27,29 +14,77 @@ Hashcash.prototype.checkToken = function (token, callback) {
     tokenString = this._tokenStringFromArray(tokenArray);
   }
   this._validateTokenArray(tokenArray);
+  return this._checkToken(tokenArray);
+};
 
+Hashcash.prototype._checkToken = function (tokenArray) {
   let difficulty = this._tokenDifficulty(tokenArray);
-  this.getHash(tokenString, function (hash) {
-    let valid = true;
-    let hashArray = Array.from(new Uint8Array(hash));
-    let numNullBytes = Math.floor(difficulty / 8);
-    for (let i = 0; i < numNullBytes; i++) {
-      if (hashArray[i] !== 0) {
-        valid = false;
-        break;
-      }
-
+  let hash = sha("sha1").update(this._tokenStringFromArray(tokenArray)).digest();
+  
+  let valid = true;
+  let hashArray = Array.from(new Uint8Array(hash));
+  let numNullBytes = Math.floor(difficulty / 8);
+  for (let i = 0; i < numNullBytes; i++) {
+    if (hashArray[i] !== 0) {
+      valid = false;
+      break;
     }
-    let numNullBits = difficulty - 8 * numNullBytes;
 
-    valid = valid && (hashArray[numNullBytes] < Math.pow(2, 8 - numNullBits));
+  }
+  let numNullBits = difficulty - 8 * numNullBytes;
+  valid = valid && (hashArray[numNullBytes] <= Math.pow(2, 8 - numNullBits));
 
-    callback(valid);
-  });
+  return valid;
 };
 
 Hashcash.prototype.generateToken = function (challenge, difficulty) {
+  let counter = 0;
+  let tokenArray = [
+      "1",
+      difficulty,
+      this._dateToString(new Date()),
+      challenge,
+      "",
+      this._randomString(),
+      0
+    ];
+    
+    while (!this._checkToken(tokenArray)) {
+      tokenArray[6] = (++counter).toString(36);
+    }
+    
+    return this._tokenStringFromArray(tokenArray);
+};
+
+Hashcash.prototype._generateToken = function (token, callback) {
+  let counter = token[6];
+  token[6] = counter.toString(36);
+  let self = this;
+  while (!this.checkToken(token)) {
+    token[6] = (counter + 1).toString(36);
+    setTimeout(() => self._generateToken(token, callback), 0);
+  }
+};
+
+Hashcash.prototype._randomString = function() {
+  return Math.floor((Math.random() * Math.pow(2, 20))).toString(36);
+
+};
+
+Hashcash.prototype._dateToString = function (date) {
+  let year = date.getFullYear().toString().substring(2);
+  let month = date.getMonth().toString();
+  month = ("00"+month).substring(month.length); // pad to 2 characters
+  let day = date.getDate().toString();
+  day = ("00"+day).substring(day.length); // pad to 2 characters
+  let hours = date.getHours().toString();
+  hours = ("00"+hours).substring(hours.length); // pad to 2 characters
+  let minutes = date.getMinutes().toString();
+  minutes = ("00"+minutes).substring(minutes.length); // pad to 2 characters
+  let seconds = date.getSeconds().toString();
+  seconds = ("00"+seconds).substring(seconds.length); // pad to 2 characters
   
+  return year + month + day + hours + minutes + seconds;
 };
 
 Hashcash.prototype._validateTokenArray = function (tokenArray) {
